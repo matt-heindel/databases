970cc869d310788f98846df7f65873f8108c9153
Zadok Kim <zadokjhkim@gmail.com>
2021-07-08 22:02:46 -0700

    made db and tables according to planned schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY,
+  username varchar(100),
+  github_handle varchar(100) DEFAULT ''
+);
+CREATE TABLE rooms (
+  room_id INTEGER PRIMARY KEY,
+  roomname varchar(100)
+);
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id integer PRIMARY KEY,
+  text varchar(255),
+  user_id integer,
+  FOREIGN KEY(user_id) REFERENCES users(user_id),
+  room_id integer,
+  FOREIGN KEY(room_id) REFERENCES rooms(room_id),
+  created_at varchar(100),
+  updated_at varchar(100)
 );
 
 /* Create other tables and define schemas for them here! */
